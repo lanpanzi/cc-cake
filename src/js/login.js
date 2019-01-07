e2f4168aaf95e1b5a8cd0ad67c7a1ff74572d4e2
lanpangzi <2551344840@qq.com>
2019-01-07 09:59:00 +0800

    refactor

--- src/js/login.js
+++ src/js/login.js
@@ -1,10 +1,8 @@
 require(["../js/requirejs.config"], () => {
 	require(["jquery", "bootstrapjs", "header", "footer"], () => {
-		var index = 0;
 		$("h3").on("click", function(){
-			index = $(this).index();
 			$(this).addClass("ac").siblings().removeClass("ac");
-			$(".content form").eq(index).addClass("ac").siblings().removeClass("ac");
+			$(".content form").eq($(this).index()).addClass("ac").siblings().removeClass("ac");
 		});
 	});
 });
